d649f0a07d050faf474772231458899c608fe8f6
Student <vlera.mustafa.11@gmail.com>
2025-11-13 15:56:08 +0100

    module 1

---
+++ Module 5/function.py
@@ -0,0 +1,50 @@
+# def greet():
+#     print("Hello World")
+#
+# greet()
+#
+# def greet_person(name):
+#     print("Hello",name)
+#
+# greet_person("Daris")
+# greet_person("Gent")
+#
+from pyexpat.errors import messages
+
+# def greet(name):
+#     message=f"Hello,{name}"
+#     print(message)
+#
+# greet("Alice")
+# print(message)
+
+# greeting="Hello"
+#
+# def greet(name):
+#     message=f"{greeting},{name}"
+#     print(message)
+#
+# greet("Bob")
+# print(greeting)
+#
+greeting="Hello"
+
+
+# def greet():
+#     global greeting
+#     greeting="Goodbye"
+#     name="Alice"
+#
+#     message=f"{greeting},{name}"
+#
+#     print(message)
+#
+# greet()
+# print(greeting)
+
+def greet_person(name,greeting="Hello"):
+    message=f"{greeting},{name}"
+    return message
+
+print(greet_person("Alice"))
+print(greet_person("Bob","Hi"))
